4b10fbb379a0a37c5d02e809ba0a22468e71eb59
anghofus <github@angho.de>
2025-10-30 17:05:35 +0100

    .

--- pygwy-txt.py
+++ pygwy-txt.py
@@ -7,7 +7,7 @@ import os
 
 
 class PygwyTxt:
-    def __init__(self, file_path: str, scan_size_x: float, scan_size_y: float, name:str=None, cmap='afmhot'):
+    def __init__(self, file_path: str, scan_size_x: float, scan_size_y: float, name:str=None, cmap='afmhot', show_plot_line=True):
         self.__file_path = file_path
         if name is None:
             self.__name = os.path.basename(os.path.splitext(self.__file_path)[0])
@@ -16,9 +16,10 @@ class PygwyTxt:
         self.__scan_size_x = scan_size_x
         self.__scan_size_y = scan_size_y
         self.__cmap = cmap
+        self.__show_plot_line = show_plot_line
         self.__scan = np.genfromtxt(file_path, delimiter='\t') * u.m
-
-        print(self.__scan.value.shape)
+        self.__profile_line_index = int(self.__scan.value.shape[0]/2)
+        self.__profile_line_distance = self.__profile_line_index * (self.__scan_size_y / self.__scan.value.shape[0])
 
         mean_exponent = int(math.floor(math.log10(self.__scan.value.mean())))
         optimal_unit_exponent = 3 * round(mean_exponent / 3)
@@ -35,6 +36,8 @@ class PygwyTxt:
 
     def plot_scan(self):
         fig, ax = plt.subplots()
+        if self.__show_plot_line:
+            plt.axhline(y=self.__profile_line_distance, color='red', linewidth=0.5)
         ax.set_title(f'{self.__name}')
         plt.imshow(self.__scan.value, cmap=self.__cmap, extent=(0, self.__scan_size_x, self.__scan_size_y, 0), interpolation='nearest')
         divider = make_axes_locatable(ax)
@@ -48,6 +51,16 @@ class PygwyTxt:
         plt.show()
         fig.savefig(os.path.join(os.path.dirname(self.__file_path), f'{self.__name}.png'), bbox_inches='tight', pad_inches=0.05, dpi=300)
 
+    def plot_profile(self):
+
+        ls = np.linspace(0, self.__scan_size_x, self.__scan.value.shape[1])
+        fig, ax = plt.subplots()
+        ax.set_title(f'{self.__name}')
+        plt.plot(ls, self.__scan[self.__profile_line])
+        plt.xlabel("width [µm]")
+        plt.ylabel(f"height [{str(self.__scan.unit)}]")
+
+        plt.show()
 
 
 scan = PygwyTxt(r"C:\Users\Mika Music\Data\251029_WNE_pygwy\gwy\ref.txt", 20, 5, cmap='viridis')
