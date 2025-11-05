16ebb8ba1efcf90db9902f4afb84c2ff82ed1d5f
anghofus <github@angho.de>
2025-11-05 12:02:53 +0100

    .

--- batch_analysis.py
+++ batch_analysis.py
@@ -1,9 +1,16 @@
 import pygwytxt
 import glob2
+import tkinter as tk
+from tkinter import filedialog
+import os
 
-basepath = r''
 
-file_list = glob2.glob('[!stats]*.txt')
+root = tk.Tk()
+root.withdraw()
+
+basepath = filedialog.askdirectory()
+
+file_list = glob2.glob(os.path.join(basepath, '*[!exclude]*.txt'))
 
 for file_path in file_list:
     scan = pygwytxt.PygwyTxt(file_path, 20, 5)
--- pygwytxt.py
+++ pygwytxt.py
@@ -8,6 +8,13 @@ from scipy.signal import find_peaks
 import json
 
 
+def homogenize_array(array):
+    max_len = max(len(row) for row in array)
+    return np.array([row + [np.nan] * (max_len - len(row)) for row in array])
+
+
+
+
 class PygwyTxt:
     def __init__(self, file_path: str, scan_size_x: float, scan_size_y: float, name:str=None):
         self.__file_path = file_path
@@ -22,6 +29,10 @@ class PygwyTxt:
         self.__distance_per_index_x = self.__scan_size_x / self.__scan.value.shape[1]
         self.__distance_per_index_y = self.__scan_size_y / self.__scan.value.shape[0]
         self.__height_map, self.__period_map = self.__generate_height_and_period_map()
+
+        self.__export_path = os.path.join(os.path.dirname(self.__file_path), 'export')
+        if not os.path.exists(self.__export_path):
+            os.mkdir(self.__export_path)
 
         mean_exponent = int(math.floor(math.log10(self.__scan.value.mean())))
         optimal_unit_exponent = 3 * round(mean_exponent / 3)
@@ -53,7 +64,7 @@ class PygwyTxt:
         ax.text(-1.7, -0.53, "µm", ha='left', va='bottom')
         plt.tight_layout()
         plt.show()
-        fig.savefig(os.path.join(os.path.dirname(self.__file_path), f'{self.__name}_heatmap.png'), bbox_inches='tight', pad_inches=0.05, dpi=300)
+        fig.savefig(os.path.join(self.__export_path, f'{self.__name}_heatmap.png'), bbox_inches='tight', pad_inches=0.05, dpi=300)
 
     def plot_profile(self):
         ls = np.linspace(0, self.__scan_size_x, self.__scan.value.shape[1])
@@ -63,7 +74,7 @@ class PygwyTxt:
         plt.xlabel("width [µm]")
         plt.ylabel(f"height [{str(self.__scan.unit)}]")
         plt.show()
-        fig.savefig(os.path.join(os.path.dirname(self.__file_path), f'{self.__name}_profile.png'), bbox_inches='tight', pad_inches=0.05, dpi=300)
+        fig.savefig(os.path.join(self.__export_path, f'{self.__name}_profile.png'), bbox_inches='tight', pad_inches=0.05, dpi=300)
 
     def plot_profile_section(self, start:int, stop:int, line:int):
         plot_line = self.__scan[line][start:stop+1]
@@ -76,40 +87,50 @@ class PygwyTxt:
         plt.xlabel("width [µm]")
         plt.ylabel(f"height [{str(self.__scan.unit)}]")
         plt.show()
-        fig.savefig(os.path.join(os.path.dirname(self.__file_path), f'{self.__name}_profile_line_{line}_from_{start}_to_{stop}.png'), bbox_inches='tight', pad_inches=0.05, dpi=300)
+        fig.savefig(os.path.join(self.__export_path, f'{self.__name}_profile_line_{line}_from_{start}_to_{stop}.png'), bbox_inches='tight', pad_inches=0.05, dpi=300)
 
     def __generate_height_and_period_map(self):
         height_list = []
         period_list = []
+
         for line in self.__scan.value:
             peaks, peak_metadata = find_peaks(line)
             valleys, valley_metadata = find_peaks(line * -1)
-            heights = line[peaks] - line[valleys]
+
+            heights = []
+            for i, peak in enumerate(peaks):
+                if i < len(valleys):
+                    height = line[peak] - line[valleys[i]]
+                    heights.append(height)
+
             periods = []
             for i in range(len(peaks)):
                 if i + 1 < len(peaks):
                     period = (peaks[i + 1] - peaks[i]) * self.__distance_per_index_x
                     periods.append(period)
+
             height_list.append(heights)
             period_list.append(periods)
 
-        height_map = np.array(height_list)
-        period_map = np.array(period_list)
+        height_map = np.array(homogenize_array(height_list))
+        period_map = np.array(homogenize_array(period_list))
+
+
 
         return height_map, period_map
 
     def __calculate_stats(self):
-        mean_height = (np.mean(self.__height_map) * u.m).to(self.__scan.unit)
-        std_height = (np.std(self.__height_map) * u.m).to(self.__scan.unit)
+        mean_height = (np.nanmean(self.__height_map) * u.m).to(self.__scan.unit)
+        std_height = (np.nanstd(self.__height_map) * u.m).to(self.__scan.unit)
 
-        mean_period = np.mean(self.__period_map) * u.um
-        std_period = np.std(self.__period_map) * u.um
+        mean_period = np.nanmean(self.__period_map) * u.um
+        std_period = np.nanstd(self.__period_map) * u.um
 
-        min_height = (float(np.min(self.__height_map)) * u.m).to(self.__scan.unit)
-        max_height = (float(np.max(self.__height_map)) * u.m).to(self.__scan.unit)
+        min_height = (float(np.nanmin(self.__height_map)) * u.m).to(self.__scan.unit)
+        max_height = (float(np.nanmax(self.__height_map)) * u.m).to(self.__scan.unit)
 
-        min_period = float(np.min(self.__period_map)) * u.um
-        max_period = float(np.max(self.__period_map)) * u.um
+        min_period = float(np.nanmin(self.__period_map)) * u.um
+        max_period = float(np.nanmax(self.__period_map)) * u.um
 
         header = f"========== {self.__name} =========="
         footer = "=" * len(header)
@@ -137,12 +158,7 @@ class PygwyTxt:
         return stats
 
     def export_stats(self):
-        export_path = os.path.join(os.path.dirname(self.__file_path), f'stats_{self.__name}.json')
+        export_path = os.path.join(self.__export_path, f'{self.__name}_stats.json')
         with open(export_path, 'w') as f:
             json.dump(self.__stats, f)
-
-
-scan = PygwyTxt(r"C:\Users\Mika Music\Data\251029_WNE_pygwy\gwy\ref.txt", 20, 5)
-
-scan.export_stats()
 
