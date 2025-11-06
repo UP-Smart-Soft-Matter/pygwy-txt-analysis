08fb3bcb4ed3a69e232a4e0270dd1d09abebf760
anghofus <github@angho.de>
2025-11-06 15:39:05 +0100

    .

--- batch_analysis.py
+++ batch_analysis.py
@@ -4,9 +4,15 @@ import os
 
 basepath = pygwy_analysis.get_file_path()
 
-file_list = glob2.glob(os.path.join(basepath, '*[!exclude]*.txt'))
-for file_path in file_list:
-    scan = pygwy_analysis.PygwyTxt(file_path, 20, 5)
+file_list_txt = glob2.glob(os.path.join(basepath, '*[!exclude]*.txt'))
+for file_path in file_list_txt:
+    scan = pygwy_analysis.PygwyTxt(file_path, 25, 25)
     scan.plot_scan()
     scan.plot_profile()
     scan.export_stats()
+
+stats = pygwy_analysis.StatJson(os.path.join(basepath, 'export'))
+stats.plot(0, 'x_max', 'px')
+stats.plot(1, 'x_max', 'px')
+stats.export_plot_data(0)
+stats.export_plot_data(1)
