25f721d9e445d0247fe508b7fd58b9c1fbeb15b1
anghofus <github@angho.de>
2026-01-12 15:43:03 +0100

    reorganized file structure, packed project into a python package

--- examles/singel_file_analysis.py
+++ examles/singel_file_analysis.py
@@ -1,12 +1,12 @@
-import pygwy_analysis
+import pygwy_txt_analysis
 
 
 scan_size_x = 25
 scan_size_y = 25
-peak_finder_settings = pygwy_analysis.PeakFinderSettings(prominence=0.3e-7)
+peak_finder_settings = pygwy_txt_analysis.PeakFinderSettings(prominence=0.3e-7)
 
-basepath = pygwy_analysis.get_file_path()
-scan = pygwy_analysis.PygwyTxt(basepath, scan_size_x, scan_size_y, peak_finder_settings=peak_finder_settings)
+basepath = pygwy_txt_analysis.get_file_path()
+scan = pygwy_txt_analysis.PygwyTxt(basepath, scan_size_x, scan_size_y, peak_finder_settings=peak_finder_settings)
 scan.plot_scan()
 scan.plot_profile()
 scan.export_stats()
